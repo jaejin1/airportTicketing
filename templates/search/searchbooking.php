4fff927a86fed89273b297299fee542bbac1c388
HK <load0117@gmail.com>
2017-12-17 00:16:23 +0900

    예매 폼 마지막 css 수정 // 예매 조회 폼 작성중

--- templates/search/searchbooking.php
+++ templates/search/searchbooking.php
@@ -1,7 +1,7 @@
 <?php
-  include_once $_SERVER['DOCUMENT_ROOT'] . '/a_team/a_team5/earthport/config/db_connect.php';
-  include_once $_SERVER['DOCUMENT_ROOT'] . '/a_team/a_team5/earthport/config/config.php';
- ?>
+include_once $_SERVER['DOCUMENT_ROOT'] . '/a_team/a_team5/earthport/config/db_connect.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/a_team/a_team5/earthport/config/config.php';
+?>
 
 <!DOCTYPE html>
 <!--
@@ -13,35 +13,50 @@ Licence URI: http://www.os-templates.com/template-terms
 -->
 <html>
 <head>
-<title>EARPORT</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<link href="../layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+	<title>EARPORT</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<link href="../layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+	<link href="../layout/styles/ticketing.css" rel="stylesheet" type="text/css" media="all">
 </head>
 <body id="top">
-<!--위의 상단바-->
-<?php
-  include GROUND_DIR . "/sideBar.php";
-?>
+	<!--위의 상단바-->
+	<?php
+	include GROUND_DIR . "/sideBar.php";
+	?>
 
-<!-- Top Background Image Wrapper -->
-<div class="bgded overlay" style="background-image:url('../images/demo/backgrounds/bg.jpg');">
-  <?php
-    include GROUND_DIR . "/header.php";
-  ?>
-</div>
-<!-- End Top Background Image Wrapper -->
+	<!-- Top Background Image Wrapper -->
+	<div class="bgded overlay" style="background-image:url('../images/demo/backgrounds/bg.jpg');">
+		<?php
+		include GROUND_DIR . "/header.php";
+		?>
+	</div>
+	<!-- End Top Background Image Wrapper -->
 
-<!--찾는 부분-->
-
-<!--footer 부분-->
-<?php
-  include GROUND_DIR . "/footer.php";
-?>
-<!-- JAVASCRIPTS -->
-<?php
-  include GROUND_DIR . "/javascriptpart.php";
-?>
+	<!--찾는 부분-->
+	<div class="searchTicketing-back">
+		<div class="searchTicketing">
+			<form id="searchFlight" action="<? echo TICKET_ROOT.'ticketbookingForm'?>" method="post" target="_blank">
+				<ui class="findTicketing">
+					<li class="whereStarting">
+						<label for="startingCity">예매 번호</label>
+						<input type="text" name="starting" placeholder="출발지" required="required" />
+					</li>
+					<button type="submit" class="btn btn-primary btn-block btn-lg">
+						<i class="fa fa-sign-in fa-2x" aria-hidden="true"></i>예매 조회하기
+					</button>
+				</ui>
+			</form>
+		</div>
+	</div>
+	<!--footer 부분-->
+	<?php
+	include GROUND_DIR . "/footer.php";
+	?>
+	<!-- JAVASCRIPTS -->
+	<?php
+	include GROUND_DIR . "/javascriptpart.php";
+	?>
 
 </body>
 </html>
